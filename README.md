2d72de295370217d8db45a96ba217ae5a4eda41c
Serafima Nazarova <sima.nazarova@gmail.com>
2023-07-03 12:17:29 +0400

    add header

--- README.md
+++
@@ -1,3 +0,0 @@
-#### Responsive landing page with React and Sass
-
-gh pages link : https://simanazarova.github.io/our-place/
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,26 @@
 import "./Header.scss";
+import React, { useState, useEffect } from "react";
+import "./Header.scss";
+import logo from "../../images/logo.png";
+import tel from "../../images/tel.svg";
 
-function Header(props) {
-  return <header className="header">hello</header>;
+function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, []);
+
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="logo" />
+      {windowWidth > 750 ? (
+        <p className="header__tel">+7 (495) 495-49-54</p>
+      ) : (
+        <img className="header__tel-img" src={tel} alt="telephone" />
+      )}
+    </header>
+  );
 }
 
 export default Header;
